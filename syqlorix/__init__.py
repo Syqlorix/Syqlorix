fdb292e6f3508c9dcec844534c037a0c7554251f
Karl Benjamin Bughaw <pepe.kotelurow@gmail.com>
2025-06-03 16:25:08 +0000

    fix: Type hinting compatibility for Python 3.8 in utils.py (v0.0.3.2 release)

--- syqlorix/__init__.py
+++ syqlorix/__init__.py
@@ -5,7 +5,7 @@ from .components import component
 from .components_lib import SimpleAlert, ImageGallery
 from .loader import load_component
 
-__version__ = "0.0.3.1"
+__version__ = "0.0.3.2"
 
 __all__ = [
     "Page", 
